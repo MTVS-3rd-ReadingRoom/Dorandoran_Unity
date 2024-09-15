c6217e02422f607f12caf5e212baaa8145b0aa71
Sim <smtlacg11@gmail.com>
2024-09-15 14:33:11 +0900

    Add Cinemachine

--- Dorandoran/Assets/02.Scripts/CinemachineManager.cs
+++ Dorandoran/Assets/02.Scripts/CinemachineManager.cs
@@ -1,6 +1,7 @@
 using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.Timeline;
@@ -8,24 +9,27 @@ using UnityEngine.Timeline;
 public class CinemachineManager : MonoBehaviour
 {
     private CinemachineBlendListCamera cinemachine;
-    public CinemachineBlendListCamera.Instruction[] instructions;
+    public CinemachineVirtualCamera[] virtualCameras;
 
     private void Awake()
     {
         cinemachine = GetComponent<CinemachineBlendListCamera>();
-    }
-    public bool test = false;
 
-    private void Update()
-    {
-        if (test)
-        {
-            test = false;
-        }   
     }
 
-    public void AddList(CinemachineVirtualCamera virtualCamera, float hold, CinemachineBlendDefinition.Style style)
+    public void AddInstructions(int index)
     {
+        List<CinemachineBlendListCamera.Instruction> instructions = new List<CinemachineBlendListCamera.Instruction>();
+        instructions.AddRange(cinemachine.m_Instructions);
+        CinemachineBlendListCamera.Instruction newInstruction = new CinemachineBlendListCamera.Instruction();
+        newInstruction.m_VirtualCamera = virtualCameras[index];
+        newInstruction.m_Hold = 0f;
+        newInstruction.m_Blend.m_Style = CinemachineBlendDefinition.Style.EaseIn;
+        newInstruction.m_Blend.m_Time = 1f;
+        instructions.Add(newInstruction);
         
+        cinemachine.m_Instructions = instructions.ToArray();
     }
+
+
 }
